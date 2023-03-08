fce576a96bd1377eb55deee2fee7f174f4a29a98
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-03-08 18:06:11 -0300

    montando um select dentro do form com react

--- src/componente/Formulario/index.js
+++ src/componente/Formulario/index.js
@@ -1,6 +1,17 @@
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSupensa';
 import "./Formulario.css";
 const Formulario = () => {
+  const times = [
+    'Programação',
+    'front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+
+  ]
   return (
     <section className='formulario'>
       <form>
@@ -8,6 +19,7 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" item={times}/>
       </form>
     </section>
   );
